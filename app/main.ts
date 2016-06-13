c85574519f5eb6766da68e5726e23237fc68a248
mayTree <nallwhy@gmail.com>
2016-06-13 20:02:16 +0900

    APP_BASE_HREF

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,10 @@
 import 'underscore/underscore-min'
 import { provide } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { APP_BASE_HREF } from '@angular/common';
 import './add-rxjs-operators';
 import { APP_CONFIG, Config, CONFIG } from './app.config';
 
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent, [provide(APP_CONFIG, { useValue: CONFIG })]);
+bootstrap(AppComponent, [provide(APP_CONFIG, { useValue: CONFIG }), provide(APP_BASE_HREF, { useValue: '/' })]);
